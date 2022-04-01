d06a7a12982932d7435771294b09cea416661b68
CardosoRepository <gabrielcard0s0@hotmail.com>
2022-04-01 10:32:23 -0300

    Tratamento de Busca Em Restaurantes

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -3,8 +3,14 @@ import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
@@ -44,8 +50,15 @@ export class RestaurantsComponent implements OnInit {
     })
 
     this.searchControl.valueChanges
-      .switchMap(searchTerm => this.restaurantsService.restaurants(searchTerm))
+      .debounceTime(500) //A busca só é realizada após 500ms depois do último evento -> Previne buscas incessantes ao banco após a digitação de cada letra
+      .distinctUntilChanged() //Emite eventos apenas se o último evento for diferente do próximo
+      // .do(searchTerm => console.log(`q = ${searchTerm}`)) Função usada para verificar os valores de pesquisa ao banco -> Era necessário apenas por motivos de consulta
+      .switchMap(searchTerm => 
+        this.restaurantsService
+        .restaurants(searchTerm)
+        .catch(error => Observable.from([])))
       .subscribe(restaurants => this.restaurants = restaurants)
+
 
     this.restaurantsService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants)
